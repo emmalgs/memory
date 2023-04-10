f4a695f5a61811be7ecc00452a702056baf8119d
Emma Gerigscott <elgerig@gmail.com>
2023-04-10 15:38:35 -0700

    WIP: card displays text, but does not flip back after click

--- src/play.js
+++ src/play.js
@@ -1,8 +1,17 @@
+import { cards } from './cards.js';
 let checkTwoCards = [];
 
 export function addCards(event) {
   console.log("clicked " + event.target.dataset.id);
   checkTwoCards.push(event.target.dataset.id);
+  if (checkTwoCards.length <= 2) {
+    cards.forEach(function(card) {
+      let cardId = Object.keys(card).join();
+      if (event.target.dataset.id === cardId) {
+        event.target.innerText = card[cardId];
+      }
+    });
+  } else { event.target.innerText = "?"; }
   checkCards();
 }
 
